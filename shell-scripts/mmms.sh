b0c96725c56fef9e9841ae5fef82cf3f21801dbf
YSL <3242809681@qq.com>
2023-10-10 15:46:32 +0800

    update

--- shell-scripts/mmms.sh
+++ shell-scripts/mmms.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 repo_url=https://gitee.com/sjtu_konglt/MMMS.git
-parent_path=MSE6701H
+parent_path=$HOME/MSE6701H
 
 # 检查是否安装git
 if [ -x "$(command -v git)" ]; then
-    mkdir $parent_path
+    mkdir ${parent_path}
 
-    git clone $repo_url $parent_path
+    cd ${parent_path}
+
+    echo -e "MMMS course materials is starting to download...\n"
+    git clone $repo_url ${parent_path}
+
+    echo -e "\nMMMS course materials has been downloaded to ${parent_path}.\n"
+
+    cd - > /dev/null
 
 else
     echo 'Error: git is not installed.' >&2
